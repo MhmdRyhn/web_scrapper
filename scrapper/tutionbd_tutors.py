c7768231c111fcc169d5f8aac75c06f4c9318c1e
Mahmood Al Rayhan <rayhan.cse12@gmail.com>
2019-02-15 22:06:04 +0600

    printing tuition post info

--- scrapper/tutionbd_tutors.py
+++ scrapper/tutionbd_tutors.py
@@ -13,17 +13,19 @@ import requests
 from bs4 import BeautifulSoup
 
 
-class TuitionInfo:
-    def __init__(self, all_tuition_url):
-        self.all_tuition_url = all_tuition_url
+class TuitionDetailsInfo:
+    def __init__(self):
+        # self.all_tuition_url = all_tuition_url
+        self.all_tuition_url = 'http://bdtutors.com/all_tuitions.html'
         self.tuition_detail_links = []
+        self.info = {}
 
     def get_html_text(self, url):
         page = requests.get(url)
         page = page.text
         return page
 
-    def extract_tuition_details_url(self):
+    def extract_tuition_details_urls(self):
         """
         Extracts the `links` of tuition details pages
 
@@ -41,11 +43,16 @@ class TuitionInfo:
                 self.tuition_detail_links.append(anchor_tag)
         return self.tuition_detail_links
 
-    def get_single_post_details(self, url):
-        post_page = self.get_html_text('http://bdtutors.com/' + url)
-        soup = BeautifulSoup(post_page, 'html.parser')
+    def get_kv(self, element, sep):
+        k, v = element.split(sep)
+        return k.strip(), v.strip()
+        # k, v = k.strip(), v.strip()
+        # self.info[k.replace(' ', '_')] = v
+
+    def get_single_post_details(self, tuition_post_page):
+        # tuition_post_page = self.get_html_text('http://bdtutors.com/' + url)
+        soup = BeautifulSoup(tuition_post_page, 'html.parser')
         element = soup.find_all('div', {'class': 'c8'})[0]
-        info = {}
         element = element.find_all('div', {'class': 'row'})
 
         # get tuition ID
@@ -53,44 +60,65 @@ class TuitionInfo:
         emt = emt.replace('\r\n\t', '')
         k, v = emt.split('#')
         k, v = k.strip(), v.strip()
-        info[k.replace(' ', '_')] = v
-        info['details'] = {}
+        self.info[k.replace(' ', '_')] = v
+
+        emt = element[1].text
+        emt = emt.strip().replace('\n', '').replace('\r', ' ')
+        # print(emt)
+        self.info['description'] = emt
+
+        self.info['details'] = {}
 
         for i in range(2, 6):
             emts = element[i]
             emts = emts.find_all('div', {'class': 'c6'})
             # pprint(emts)
             emt1, emt2 = emts[0].text, emts[1].text
-            print(emt1, emt2)
-            print('-' * 20)
-            k, v = emt1.split(':')
-            k, v = k.strip(), v.strip()
-            info['details'][k.replace(' ', '_')] = v
-            k, v = emt2.split(':')
-            k, v = k.strip(), v.strip()
-            info['details'][k.replace(' ', '_')] = v
+            # print(emt1, emt2)
+            # print('-' * 20)
+            k, v = self.get_kv(emt1, ':')
+            self.info['details'][k.replace(' ', '_')] = v
+            k, v = self.get_kv(emt2, ':')
+            self.info['details'][k.replace(' ', '_')] = v
         emt = element[6]
-        print('emt:', emt)
+        # print('emt:', emt)
         emt = emt.find_all('div', {'class': 'c6'})[1]
         emt = emt.text
-        k, v = emt.split(':')
-        k, v = k.strip(), v.strip()
-        print('v:', v)
-        info['details'][k.replace(' ', '_')] = v
+        k, v = self.get_kv(emt, ':')
+        self.info['details'][k.replace(' ', '_')] = v
 
-        return info
+        pprint(self.info)
+        return self.info
+
+    def aggregate_pages(self, url_list):
+        html_pages = []
+        for url in url_list:
+            page = self.get_html_text('http://bdtutors.com/' + url)
+            html_pages.append(page)
+        return html_pages
 
     def get_tuition_post_details(self):
-        tuition_details_link = self.extract_tuition_details_url()
+        tuition_details_link = self.extract_tuition_details_urls()
+        pprint(tuition_details_link)
+        all_details_page = self.aggregate_pages(tuition_details_link)
+        info_list = []
+        for page in all_details_page:
+            info = self.get_single_post_details(page)
+            info_list.append(info)
+        return info_list
 
 
 if __name__ == '__main__':
-    tuition_detail_info_links = TuitionInfo('asd').get_single_post_details(
-        'index.php?cms=tuitiondetails&id=2092019100925'
-    )
-    # tuition_detail_info_links = TuitionInfo('http://bdtutors.com/all_tuitions.html').extract_tuition_details_url()
-    # print(len(tuition_detail_info_links))
-    pprint(tuition_detail_info_links)
+    info_of_today = TuitionDetailsInfo().get_tuition_post_details()
+    # pprint(info_of_today)
+    # print(len(info_of_today))
+    d = {
+        'all_info': info_of_today
+    }
+    import json
+
+    with open('today.json', 'w') as file:
+        json.dump(d, file, indent=4)
 
 """
 http://bdtutors.com/all_tuitions.html
